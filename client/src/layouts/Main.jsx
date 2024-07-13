d723ca0195f67e8f60f027ead2ffe4e2314bb39f
Ahsan Tonmoy <ahosant82@gmail.com>
2024-07-13 18:28:04 +0600

    ui Project setup

---
+++ client/src/layouts/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            vhjkdfhk
+        </div>
+    );
+};
+
+export default Main;
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            this is inventory
+        </div>
+    );
+};
+
+export default Home;
